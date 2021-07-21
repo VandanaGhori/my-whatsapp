f9710baaae1f19031c7f56e86a52fea23b36df35
VandanaGhori <vandanaghori19@gmail.com>
2021-07-21 13:59:40 -0400

    User profile picture updated successfully in settings activity.

--- app/src/main/java/com/example/mywhatsapp/SettingsActivity.java
+++ app/src/main/java/com/example/mywhatsapp/SettingsActivity.java
@@ -1,16 +1,37 @@
 package com.example.mywhatsapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.mywhatsapp.Models.Users;
 import com.example.mywhatsapp.databinding.ActivitySettingsBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.net.URI;
 
 public class SettingsActivity extends AppCompatActivity {
 
     ActivitySettingsBinding binding;
+    FirebaseStorage storage;
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +46,67 @@ public class SettingsActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        storage = FirebaseStorage.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        // For fetching already set profile picture from the firebase database
+        database.getReference().child("Users").child(FirebaseAuth.getInstance().getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        Users user = snapshot.getValue(Users.class);
+                        Picasso.get().load(user.getProfilePic()).placeholder(R.drawable.user).into(binding.profileImage);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+        binding.addProfilePicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(intent.ACTION_GET_CONTENT);
+                intent.setType("image/*"); // */*
+                startActivityForResult(intent,33);
+            }
+        });
+
         //getSupportActionBar().hide();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // If user has selected any image for updating profilePic then it will set to the ProfilePic container
+        if(requestCode == 33 && data.getData() != null) {
+            Uri sFile = data.getData();
+            binding.addProfilePicButton.setImageURI(sFile);
+
+            // Storing image source into the firebase under "profile_pictures" title an inside there is a node named as user's id
+            final StorageReference reference = storage.getReference().child("profile_pictures")
+                    .child(FirebaseAuth.getInstance().getUid());
+
+            // store image into firebase storage
+            reference.putFile(sFile).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // It will give url of the stored image from Firebase Storage snd save them into firebase database
+                    reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            database.getReference().child("Users").child(FirebaseAuth.getInstance().getUid())
+                                    .child("profilePic").setValue(uri.toString());
+                            Toast.makeText(SettingsActivity.this, "Profile Picture Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            });
+        }
     }
 }
